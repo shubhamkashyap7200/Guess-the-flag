306f31941dca06ccf961030c0f1a8f2382a69326
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-07-21 10:59:38 +0530

    made progress

--- Guess the flag/AlertSwiftUIView.swift
+++ Guess the flag/AlertSwiftUIView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct AlertSwiftUIView: View {
+    @State private var showingAlert = false
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button("Show Alert") {
+                showingAlert = true
+            }
+            .alert("Important Message", isPresented: $showingAlert) {
+                Button("OK") {}
+                Button("Cancel", role: .cancel) {}
+                Button("Delete", role: .destructive) {}
+            } message: {
+                Text("Test message here")
+            }
+        }
     }
 }
 
